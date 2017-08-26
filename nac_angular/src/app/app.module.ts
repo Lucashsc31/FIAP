cc88e511575de1bda5c9f2972e442cd45e6a0ec2
Nathalia <nathy_cardoso10@hotmail.com>
2017-08-26 15:49:41 -0300

    nac angular agosto/2017

---
+++ nac_angular/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {HttpModule} from '@angular/http'
+import {RouterModule} from '@angular/router'
+
+import {ROUTES} from './app.routes'
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AlunosComponent } from './alunos/alunos.component';
+import { DisciplinasComponent } from './disciplinas/disciplinas.component';
+import { ContatoComponent } from './contato/contato.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AlunosComponent,
+    DisciplinasComponent,
+    ContatoComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
